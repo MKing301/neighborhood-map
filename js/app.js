3a3a67b16573c29b31accaafa6d081603f2388a4
MKING301 <mking301@gmail.com>
2018-08-21 17:44:14 -0400

    Added phone number view model

--- js/app.js
+++ js/app.js
@@ -1,30 +1,37 @@
 var viewModel = {
   places: [ {
 		title: 'Saint Louis Zoo',
+		phone: '(314) 781-0900',
 		location: {lat: 38.6385721, lng: -90.2938032}
 	},
 	{
-		title: 'The Gateway Arch', 
+		title: 'The Gateway Arch',
+		phone: '(877) 982-1410',
 		location: {lat: 38.624691, lng: -90.186965}
 	},
 	{
-		title: 'The Delmar Loop', 
+		title: 'Delmar Loop',
+		phone: '(314) 727-8000',
 		location: {lat: 38.6560226, lng: -90.3075077}
 	},
 	{
 		title: 'Saint Louis Art Museum',
+		phone: '(314) 721-0072',
 		location: {lat: 38.6390952, lng: -90.2971418}
 	},
 	{
 		title: 'Missouri History Museum',
+		phone: '(314) 746-4599',
 		location: {lat: 38.6452478, lng: -90.287927}
 	},
 	{
 		title: 'Missouri Botanical Garden',
+		phone: '(314) 577-5100',
 		location: {lat: 38.6161054, lng: -90.260256}
 	},
 	{
 		title: 'Ted Drewes Frozen Custard',
+		phone: '(314) 481-2652',
 		location: {lat: 38.589412, lng: -90.307809}
 	} ]
 };
@@ -35,9 +42,9 @@ viewModel.searchResults = ko.computed(function() {
     var q = viewModel.Query();
     return viewModel.places.filter(function(i) {
       return i.title.toLowerCase().indexOf(q) >= 0;
-    });
+    }); 
 });
-	
+
 
 $(document).ready(function() {
 	$('.toggle-button').click(function() {
